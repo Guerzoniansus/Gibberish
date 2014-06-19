1de4dde6c6e4f8c2671c937647879e0bbc3c0787
seawee65 <seawee65@live.com>
2014-06-19 15:01:50 -0400

    yay

--- src/NomarTheHero/ChatListener.java
+++ src/NomarTheHero/ChatListener.java
@@ -1,31 +1,36 @@
 package NomarTheHero;
 
+import java.util.HashMap;
+
 import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
 
 public class ChatListener implements Listener {
-	
-	public void onChat(AsyncPlayerChatEvent event){
-		
-		Player player = event.getPlayer();
-		String message = event.getMessage();
-		
-		
-		if (!Gibberish.hasEnabled.isEmpty()){
-			if (Gibberish.hasEnabled.contains(player.getName())){
-				
-				message.replaceAll("(?i)there", "der");
-				message.replaceAll("(?i)i am", "i iz");
-								
-				
-				
-				
-				event.setMessage(message);
-			}
-		}
-					
+
+	private HashMap<String, String> words;
+
+	public ChatListener(HashMap<String, String> words) {
+		this.words = words;
+
 	}
 
+	public void onChat(AsyncPlayerChatEvent event) {
 
+		Player player = event.getPlayer();
+		String message = event.getMessage();
+
+		if (!Gibberish.hasEnabled.isEmpty()) {
+			if (Gibberish.hasEnabled.contains(player.getName())) {
+				for (String word : words.keySet()) {
+
+					message.replace(word, words.get(word));
+
+				}
+
+				event.setMessage(message);
+
+			}
+		}
+	}
 }
--- src/NomarTheHero/Gibberish.java
+++ src/NomarTheHero/Gibberish.java
@@ -1,5 +1,6 @@
 package NomarTheHero;
 
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -9,50 +10,58 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
-
 public class Gibberish extends JavaPlugin {
-	
+
 	public static Set<String> hasEnabled = new HashSet<String>();
-    
-    public void onEnable(){
-        getServer().getPluginManager().registerEvents(new ChatListener(), this);
-    }
-    
-    public void onDisable() {
-    }
-    
-    
-    @Override
-    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-    	if (command.getName().equalsIgnoreCase("gibberish")){
-    		if (sender instanceof Player){
-    			
-    			Player player = (Player)sender;
-    			
-    			if (args.length != 0){
-        			player.sendMessage(ChatColor.RED + "Usage: /gibberish");
-        		} 
-    			
-    			else {
-        			
-        			if (hasEnabled.contains(player.getName())){
-        				hasEnabled.remove(player.getName());
-        				player.sendMessage(ChatColor.BLUE + "Gibberish disabled!");
-        			} 
-        			
-        			else {
-        				hasEnabled.add(player.getName());
-        				player.sendMessage(ChatColor.BLUE + "Gibberish enabled!");
-        			}       			
-        			
-        		}  			
-    			
-    		}    		
-    	}
 
-    	return true;
-    }
+	public static HashMap<String, String> words = new HashMap<String, String>();
 
-    
+	public void onEnable() {
+
+		// register wuuuuurds
+
+		registerWord("there", "der");
+
+		getServer().getPluginManager().registerEvents(new ChatListener(words), this);
+	}
+
+	public void onDisable() {
+	}
+
+	@Override
+	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+		if (command.getName().equalsIgnoreCase("gibberish")) {
+			if (sender instanceof Player) {
+
+				Player player = (Player) sender;
+
+				if (args.length != 0) {
+					player.sendMessage(ChatColor.RED + "Usage: /gibberish");
+				}
+
+				else {
+
+					if (hasEnabled.contains(player.getName())) {
+						hasEnabled.remove(player.getName());
+						player.sendMessage(ChatColor.BLUE + "Gibberish disabled!");
+					}
+
+					else {
+						hasEnabled.add(player.getName());
+						player.sendMessage(ChatColor.BLUE + "Gibberish enabled!");
+					}
+
+				}
+
+			}
+		}
+
+		return true;
+	}
+
+	public void registerWord(String regularWord, String gibberishWord) {
+		words.put("(?i)" + regularWord, gibberishWord);
+
+	}
 
 }
